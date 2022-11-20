81db14f7165db5dfe8f30a0c0a59c40aca5d29b1
neobis2022team2autnm <project.autumn.2022.team2@gmail.com>
2022-11-20 18:32:52 +0600

    add new section

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import Navbar from "./components/Navbar";
 import Navigation from './components/Navigation';
 import SearchBar from './components/SearchBar';
 import {
-  regions, articles
+  regions, articles, foodPlace, reluxPlace
 } from "../src/api/mock-data/data";
 import Slider from "./components/Slider";
 import Albom from "./components/Albom";
+import Lifehack from "./components/Lifehack";
 function App() {
 
   return (
@@ -16,6 +17,9 @@ function App() {
       <Slider title={'Регионы'} desc={'Каждая регион богат красивой природой и историей'} data={regions} />
       <Slider title={'Блоги'} desc={'Лучшие блоги наших пользователей на S-TRIP'} data={articles} />
       <Albom />
+      <Slider title={'Кафе и рестораны'} desc={'Места где готовят самую вкусную еду'} data={foodPlace} />
+      <Lifehack />
+      <Slider title={'Отели и хостелы'} desc={'Места отдохнуть или переночевать'} data={reluxPlace} />
     </div>
   );
 }
--- src/api/mock-data/data.js
+++ src/api/mock-data/data.js
@@ -57,3 +57,58 @@ export const articles = [
     text: 'Иссыкуль'
   },
 ]
+
+export const foodPlace = [
+  {
+    id: 1,
+    img: './foodPlace/supara.png',
+    text: 'Супара Этно-комплекс'
+  },
+  {
+    id: 2,
+    img: './foodPlace/faiza.png',
+    text: 'Фаиза',
+  },
+  {
+    id: 3,
+    img: './foodPlace/navat.png',
+    text: 'Чайхана Navat'
+  },
+  {
+    id: 4,
+    img: './foodPlace/chaina.png',
+    text: 'China Town'
+  },
+  {
+    id: 5,
+    img: './foodPlace/cloud.png',
+    text: 'Облако 53'
+  },
+]
+export const reluxPlace = [
+  {
+    id: 1,
+    img: './reluxPlace/orion.png',
+    text: 'Orion Hotel'
+  },
+  {
+    id: 2,
+    img: './reluxPlace/sheraton.png',
+    text: 'Sheraton'
+  },
+  {
+    id: 3,
+    img: './reluxPlace/novotel.png',
+    text: 'Novotel'
+  },
+  {
+    id: 4,
+    img: './reluxPlace/jannat.png',
+    text: 'Jannat Regency'
+  },
+  {
+    id: 5,
+    img: './reluxPlace/hyat.png',
+    text: 'Hyatt Regency'
+  },
+]
