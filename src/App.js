2286527cb25e4d366b109035254e557086db996e
neobis2022team2autnm <project.autumn.2022.team2@gmail.com>
2022-11-20 18:07:29 +0600

    init create album section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   regions, articles
 } from "../src/api/mock-data/data";
 import Slider from "./components/Slider";
+import Albom from "./components/Albom";
 function App() {
 
   return (
@@ -14,6 +15,7 @@ function App() {
       <SearchBar />
       <Slider title={'Регионы'} desc={'Каждая регион богат красивой природой и историей'} data={regions} />
       <Slider title={'Блоги'} desc={'Лучшие блоги наших пользователей на S-TRIP'} data={articles} />
+      <Albom />
     </div>
   );
 }
---
+++ src/components/Albom/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import './style.scss'
+
+const Albom = () => {
+  return (
+    <div className="album">
+      <div className="album__container">
+        <div className="max-w-[100%] mx-auto p-10">
+          <div className="max-h-[500px] relative">
+            <div className="p-25px absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-end">
+              <h1 className="px-4 text-1xl sm:text-1xl md:text-2xl lg:text-1xl font-bold">
+                Вдохновляющие фото альбомы в S-TRIP
+              </h1>
+              <button className="album__button my-10px rounded-full w-48 bg-light hover:bg-black-700 text-white font-bold py-2 px-6 border border-black-700 rounded py-2 px-4">
+                Узнать больше
+              </button>
+            </div>
+            <img
+              className="w-full max-h-[350px] object-cover"
+              src="https://triptokyrgyzstan.com/sites/default/files/images/2019-04/slide-1960x857-07.jpg"
+              alt="/"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Albom;
