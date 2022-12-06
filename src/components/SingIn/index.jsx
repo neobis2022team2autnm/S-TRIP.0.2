72714938b0a51529be22fbdc33c408e7bdfd7132
codeSlayer7 <bahazve01@gmail.com>
2022-12-06 15:23:15 +0600

    add: interceptor

--- src/components/SingIn/index.jsx
+++ src/components/SingIn/index.jsx
@@ -1,50 +1,4 @@
-// //import React and additional dependencies
-// import { useEffect, useRef } from "react";
 
-// //import components
-// import { LoginByGoogle } from "../../port/index";
-
-// export const Signin = ({ status, active, setActive }) => {
-//   const googleBtn = useRef(null);
-
-//   const handleCallbackResponse = (response) => {
-//     LoginByGoogle(response.credential);
-//   };
-
-//   useEffect(() => {
-//     /*global google */
-//     google.accounts.id.initialize({
-//       client_id:
-//         "389304183889-m72ksip97kttaov9jkd86anlkkgiphgh.apps.googleusercontent.com",
-//       callback: handleCallbackResponse,
-//     });
-
-//     google.accounts.id.renderButton(googleBtn.current, {
-//       theme: "outline",
-//       size: "large",
-//     });
-
-//   }, []);
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     setActive(false)
-//   };
-
-//   return (
-//     <form onClick={handleSubmit}>
-//       <div className={"google_login"}>
-//         <button ref={googleBtn} />
-//       </div>
-//     </form>
-//   );
-// };
-
-// export default Signin;
-
-
-// import logo from './logo.svg';
-// import './App.css';
 import { GoogleLogin } from '@react-oauth/google';
 import jwt_decode from "jwt-decode";
 import { useGoogleLogin } from '@react-oauth/google';
@@ -53,7 +7,7 @@ import axios from "axios"
 
 function App() {
 
-  const login = useGoogleLogin({
+  const Login = useGoogleLogin({
     onSuccess: async respose => {
       try {
         const res = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
@@ -62,7 +16,7 @@ function App() {
           }
         })
 
-        console.log(res.data)
+        console.log('не понятный кусок кода',res.data)
       } catch (err) {
         console.log(err)
 
@@ -79,7 +33,7 @@ function App() {
     <div className="App">
       <header className="App-header flex justify-center ">
         <GoogleLogin
-          onClick={login}
+          onClick={Login}
           onSuccess={onSuccess}
           onError={() => {
             console.log('Login Failed');
--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from 'axios';
+import TokenService from "../service/userService/tokenService";
 
 const API = axios.create({
   baseURL: "http://34.159.147.205",
@@ -11,11 +12,16 @@ const API = axios.create({
 export const auth = createAsyncThunk(
   "auth/auth",
   async (data, { rejectWithValue }) => {
+    console.log(data);
     try {
       const response = await API.post("/users/google/", {
         auth_token: data
       });
-      console.log(response);
+      if(response.data.tokens){ 
+        console.log(response)
+        TokenService.setUser(response.data.tokens)
+      }
+      
       return response.data;
     } catch (e) {
       console.log(e);
